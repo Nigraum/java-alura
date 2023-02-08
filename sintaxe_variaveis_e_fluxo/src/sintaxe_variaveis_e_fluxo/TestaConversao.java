aa5b58c7878cc727537cf92a00e25c630cd7c745
Nigraum <yan.corintima@hotmail.com>
2023-02-08 19:38:42 -0300

    Learning float and long

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaConversao.java
@@ -0,0 +1,20 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		
+		double salario = 1270.50;
+		
+		float pontoFlutuante = 3.14f;
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 312321312312421412l;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println(total);
+	}
+}
