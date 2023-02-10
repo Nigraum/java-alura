1202bc1747460ee0084e01b687fc3f1adc13ce86
Nigraum <yan.corintima@hotmail.com>
2023-02-10 16:16:17 -0300

    Learning char and string]

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaCaracteres.java
@@ -0,0 +1,14 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaCaracteres {
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 65;
+		System.out.println(valor);
+		
+		String palavra = "Test testando";
+		System.out.println(palavra);
+	}
+}
