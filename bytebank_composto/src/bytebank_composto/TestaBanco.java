7cd970d3ff235d081fd516bee9c3635b59bb5c7a
Nigraum <yan.corintima@hotmail.com>
2023-02-16 19:27:36 -0300

    Create TestaBanco class

---
+++ bytebank_composto/src/bytebank_composto/TestaBanco.java
@@ -0,0 +1,10 @@
+package bytebank_composto;
+
+public class TestaBanco {
+	public static void main(String[] args) {
+		Cliente paulo = new Cliente();
+		paulo.nome = "Paulo Silveira";
+		paulo.cpf = "222.222.222-22";
+		paulo.profissao = "Programado";
+	}
+}
