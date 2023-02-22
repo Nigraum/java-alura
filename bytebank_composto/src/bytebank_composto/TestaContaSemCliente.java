b18b365d752df1ab40c476fb552512442575c6bb
Nigraum <yan.corintima@hotmail.com>
2023-02-22 18:22:53 -0300

    Learning null references

---
+++ bytebank_composto/src/bytebank_composto/TestaContaSemCliente.java
@@ -0,0 +1,13 @@
+package bytebank_composto;
+
+public class TestaContaSemCliente {
+	public static void main(String[] args) {
+		Conta contaDaMarcela =  new Conta();
+		System.out.println(contaDaMarcela.saldo);
+		
+		contaDaMarcela.titular = new Cliente();
+		
+		contaDaMarcela.titular.nome = "Marcela";
+		System.out.println(contaDaMarcela.titular.nome);
+	}
+}
