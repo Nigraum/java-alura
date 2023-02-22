04ee008099f559a3762ce4dabff0f11bdfaf35ba
Nigraum <yan.corintima@hotmail.com>
2023-02-22 19:01:11 -0300

    Learning to use get and set

--- bytebank_encapsulado/src/bytebank_encapsulado/Conta.java
+++ bytebank_encapsulado/src/bytebank_encapsulado/Conta.java
@@ -37,7 +37,15 @@ public class Conta {
 		return this.numero;
 	}
 	
-	public void setNumero(int novoNumero) {
-		this.numero = novoNumero;
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
 	}
 }
--- bytebank_encapsulado/src/bytebank_encapsulado/TestaGeteSetter.java
+++ bytebank_encapsulado/src/bytebank_encapsulado/TestaGeteSetter.java
@@ -1,5 +1,9 @@
 package bytebank_encapsulado;
 
 public class TestaGeteSetter {
-
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+	}
 }
