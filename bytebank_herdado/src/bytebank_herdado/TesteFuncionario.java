78ab00f0e4e19f7653e54f21f2b7b72aa2fa7857
Nigraum <yan.corintima@hotmail.com>
2023-02-23 19:43:15 -0300

    Create class testafuncionario

---
+++ bytebank_herdado/src/bytebank_herdado/TesteFuncionario.java
@@ -0,0 +1,17 @@
+package bytebank_herdado;
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario paulo = new Funcionario();
+		paulo.setNome("Paulo Silveira");
+		paulo.setCpf("222.222.222-22");
+		paulo.setSalario(2600.0);
+		
+		System.out.println(paulo.getNome());
+		System.out.println(paulo.getBonificao());
+		
+	}
+
+}
